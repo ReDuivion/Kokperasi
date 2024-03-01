5a76c6b5aa6c957d2f282358212c5319a729e917
Ririena <nerllysurina@gmail.com>
2024-03-01 22:48:07 +0700

    First Commit Init

---
+++ resources/src/App.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+    return (
+        <>
+            <main>
+                <section>
+                    <div>
+                        <h1 className="text-2xl text-blue-600">Hello World</h1>
+                    </div>
+                </section>
+            </main>
+        </>
+    );
+}
